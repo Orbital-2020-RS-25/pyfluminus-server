5195ad86bd08407447649ec3ec4ab6c8002ee1cd
w-yuchen <yuchenwang97@gmail.com>
2020-05-22 13:02:36 +0800

    Added mods table

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from app import db
 
-class User(db.model):
+class User(db.Model):
     #database id
     id = db.Column(db.Integer, primary_key=True)
     #name of user, not unique as people may have same names
@@ -10,3 +10,14 @@ class User(db.model):
 
     def __repr__(self):
         return "<User {}>".format(self.name)
+
+class User_Mods(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(8), index=True, unique=False)
+    mod_id = db.Column(db.String(64), index=True, unique=False)
+    class_grp = db.Column(db.String(6), index=True, unique=False, default="T0")
+    term = db.Column(db.String(6), index=True, unique=False)
+    student = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return "<Mod {} taken by {}>".format(self.code, self.student)
--- app/user.py
+++ app/user.py
@@ -1,3 +1,5 @@
+from app import db
+
 class User:
     def __init__(self, netId, name, mods, term):
         self.netId = netId
@@ -5,5 +7,4 @@ class User:
         self.mods = mods
         self.term = term
 
-    def getName():
-        return self.name
+    
