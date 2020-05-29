2afa7bc6b156078a5843a8f7e51e570bff0951c9
w-yuchen <yuchenwang97@gmail.com>
2020-05-29 18:24:15 +0800

    edits to models

--- app/models.py
+++ app/models.py
@@ -49,7 +49,7 @@ class User_Mods(db.Model):
     name = db.Column(db.String(30))
     class_grp = db.Column(db.String(6), index=True, unique=False, default="T0")
     term = db.Column(db.String(6), index=True, unique=False)
-    student = db.Column(db.String, db.ForeignKey('users.nus_net_id'))
+    student = db.Column(db.String, db.ForeignKey('user.nus_net_id'))
 
     def __repr__(self):
         return "<Mod {} taken by {}>".format(self.code, self.student)
